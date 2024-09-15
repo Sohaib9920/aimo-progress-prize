1e34741f09d5ec2ae67337595c344a5e481f71ce
Sohaib9920 <sohaib9920.ahmed@gmail.com>
2024-09-15 22:29:51 +0500

    model config fix

--- training/aimo/configs/model_config.py
+++ training/aimo/configs/model_config.py
@@ -19,10 +19,11 @@ from transformers import MODEL_FOR_CAUSAL_LM_MAPPING
 
 MODEL_CONFIG_CLASSES = list(MODEL_FOR_CAUSAL_LM_MAPPING.keys())
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
+import trl
 
 
 @dataclass
-class ModelConfig:
+class ModelConfig(trl.ModelConfig):
     """
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune, or train from scratch.
     """
